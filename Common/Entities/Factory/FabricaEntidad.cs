ff9ddfe3af875bb5610ce6059a2417e09dd57b05
jesus yepes <jesusyepes.1205@gmail.com>
2021-06-04 00:11:01 -0400

    Se crea la carpeta compartida para los recursos y entidades comunes

---
+++ Common/Entities/Factory/FabricaEntidad.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationBackend.Common.Entities.Factory
+{
+    /// <summary>
+    /// Patron Fabrica
+    /// </summary>
+    public static class FabricaEntidad
+    {
+        /// <summary>
+        /// Metodo que realiza una instancia de tipo aspirante : Patron Fabrica
+        /// </summary>
+        /// <returns></returns>
+        public static Aspirante CrearAspirante()
+        {
+            return new Aspirante();
+        }
+
+        /// <summary>
+        /// Metodo que realiza una instancia de tipo aspirante insertando los datos: Patron Fabrica
+        /// </summary>
+        /// <returns></returns>
+        public static Aspirante CrearAspirante(int identificacion, string nombre, string apellido,
+                                                       int edad, string nombreCasa)
+        {
+            return new Aspirante(identificacion, nombre, apellido, edad, nombreCasa);
+        }
+    }
+}
